a10c4d9656ddbc862b515e3e0ad42285d97b8aec
shravanikonde03 <153658184+shravanikonde03@users.noreply.github.com>
2023-12-26 12:51:11 +0530

    Updates

---
+++ precache-manifest.4db73e78b46292edf8f67d1ac188a627.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e8b21249cc1d7493196",
+    "url": "/portfolio/static/css/main.226f89ed.chunk.css"
+  },
+  {
+    "revision": "2e8b21249cc1d7493196",
+    "url": "/portfolio/static/js/main.3cafb4b6.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "d4f719345186628d7ca3",
+    "url": "/portfolio/static/js/2.070aa402.chunk.js"
+  },
+  {
+    "revision": "9571895b144e28775fd433e6e51b073e",
+    "url": "/portfolio/static/media/python.9571895b.svg"
+  },
+  {
+    "revision": "d9237725db7f9cfd32560a8a93505d4b",
+    "url": "/portfolio/static/media/javascript.d9237725.svg"
+  },
+  {
+    "revision": "6a3bfe5d1247b2d620debf98f4b6dc7a",
+    "url": "/portfolio/static/media/mongodb.6a3bfe5d.svg"
+  },
+  {
+    "revision": "7cce028487f05a5e5e778e013a0b4ce5",
+    "url": "/portfolio/static/media/mysql.7cce0284.svg"
+  },
+  {
+    "revision": "b6dd61c9a165e83bdfd07e9fb07d3889",
+    "url": "/portfolio/static/media/css.b6dd61c9.svg"
+  },
+  {
+    "revision": "521bdc40ebd94461995a83048f9dde23",
+    "url": "/portfolio/static/media/html.521bdc40.svg"
+  },
+  {
+    "revision": "084d93c3f49e00957f2e7779cfc79791",
+    "url": "/portfolio/static/media/crunch.084d93c3.png"
+  },
+  {
+    "revision": "78ff0d17e4edde05b60b39abf5f10c3c",
+    "url": "/portfolio/static/media/chaptersqueue.78ff0d17.png"
+  },
+  {
+    "revision": "49407d153adbc03ea57d7058e74d8349",
+    "url": "/portfolio/static/media/fyp.49407d15.png"
+  },
+  {
+    "revision": "4debec15f379fb5a2d2fb0d77fe1bce0",
+    "url": "/portfolio/static/media/aboutme2.4debec15.png"
+  },
+  {
+    "revision": "6d25ae389f5f8ecf7f9dcb80076de116",
+    "url": "/portfolio/static/media/c.6d25ae38.svg"
+  },
+  {
+    "revision": "c95c3642f8714cd2c01996eb17690632",
+    "url": "/portfolio/static/media/c++.c95c3642.svg"
+  },
+  {
+    "revision": "2bc1093d824833bdcf86f8cad530246a",
+    "url": "/portfolio/static/media/java.2bc1093d.svg"
+  },
+  {
+    "revision": "9b974cf84270c122d370f45b6aba0878",
+    "url": "/portfolio/index.html"
+  }
+];
